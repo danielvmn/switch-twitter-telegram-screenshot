9e1c4f17394390d4d7e808a3cdbb6646c770aee9
Daniel Negrao <danielvmn@gmail.com>
2020-03-30 14:49:06 +0200

    First comit

---
+++ download_screenshot.py
@@ -0,0 +1,36 @@
+import tweepy
+from decouple import config
+from telegram import Bot
+
+# Twitter authentication
+twitter_api_key = config('twitter_api_key')
+twitter_api_secret_key = config('twitter_api_secret_key')
+twitter_access_token = config('twitter_access_token')
+twitter_access_token_secret = config('twitter_access_token_secret')
+twitter_filter = config('twitter_filter')
+
+# Telegram authentication
+telegram_api_token = config('telegram_api_token')
+telegram_group = config('telegram_group')
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, tweet):
+        print ('Receiving tweet: ' + tweet.text)
+        if 'media' in tweet.entities:
+            print('Saving Image')
+            img = tweet.entities['media'][0]['media_url_https']
+            bot.send_photo(chat_id=telegram_group, photo=img)
+        else:
+            print("Tweet doesn't contain an image")
+
+
+# Telegram bot startup
+bot = Bot(token=telegram_api_token)
+
+# Twitter stream startup
+auth = tweepy.auth.OAuthHandler(twitter_api_key, twitter_api_secret_key)
+auth.set_access_token(twitter_access_token, twitter_access_token_secret)
+stream = tweepy.Stream(auth, MyStreamListener(), timeout=None)
+# Twitter account to filter
+stream.filter(follow=[twitter_filter])
