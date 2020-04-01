57e21b710abe56ba69773d46af214e366c6f5e30
Daniel Negrao <danielvmn@gmail.com>
2020-04-01 15:31:34 +0200

    Basic exception handling

--- download_screenshot.py
+++ download_screenshot.py
@@ -16,19 +16,21 @@ telegram_group = config('telegram_group')
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, tweet):
-        print ('Receiving tweet: ' + tweet.text)
-        if 'media' in tweet.entities:
-            if 'video' in tweet.extended_entities['media'][0]['type']:
-                print('Saving Video')
-                vid = tweet.extended_entities['media'][0]['video_info']['variants'][1]['url']
-                bot.send_video(chat_id=telegram_group, video=vid)
+        try:
+            print ('Receiving tweet: ' + tweet.text)
+            if 'media' in tweet.entities:
+                if 'video' in tweet.extended_entities['media'][0]['type']:
+                    print('Saving Video')
+                    vid = tweet.extended_entities['media'][0]['video_info']['variants'][1]['url']
+                    bot.send_video(chat_id=telegram_group, video=vid)
+                else:
+                    print('Saving Image')
+                    img = tweet.entities['media'][0]['media_url_https']
+                    bot.send_photo(chat_id=telegram_group, photo=f'{img}:large')
             else:
-                print('Saving Image')
-                img = tweet.entities['media'][0]['media_url_https']
-                bot.send_photo(chat_id=telegram_group, photo=f'{img}:large')
-        else:
-            print("Tweet doesn't contain an image")
-
+                print("Tweet doesn't contain an image")
+        except Exception as e:
+            print(e)
 
 # Telegram bot startup
 bot = Bot(token=telegram_api_token)
